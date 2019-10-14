ae3254267608d666bdcd4ad3cf435f6f848479b7
TungHuynh <tunghuynh@TungHuynhs-MacBook-Pro.local>
2019-10-14 22:34:52 +0700

    Update server.js
    bodyParser.json()

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ var bodyParser = require('body-parser');
 var express = require('express');
 
 var app = express();
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.json());
 var server = http.createServer(app);
 var request = require("request");
 
